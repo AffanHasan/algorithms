2911b0a4d3862e2d59aefe878b71847821c7a31f
Affan Hassan <ahassan@nis-pk549s-mbp.home>
2024-03-12 14:28:25 +0500

    Added binary search for RUST

---
+++ binary-search.rs
@@ -0,0 +1,29 @@
+use std::io;
+use std::cmp::Ordering;
+
+fn main() {
+    // a sorted list of names
+    let names: [&str; 5] = ["Aazim", "Abdullah", "Abid", "Affan", "Ahmed"];
+    let mut name: String = String::new();
+    println!("Provide The Name To Search! ");
+    io::stdin()
+        .read_line(&mut name)
+        .expect("Failed To Read");
+    name = name.trim().to_string();
+    let mut low: usize = 0;
+    let mut high: usize = 4;
+    let mut middle: usize = 0;
+    while low <= high {
+        middle = (low + high) / 2;
+        if &name[..] < names[middle] {
+            high = middle - 1;
+        } else if &name[..] > names[middle] {
+            low = middle + 1;
+        } else if &name[..] == names[middle] {
+            println!("Name: {name} found at index {middle}");
+            break;
+        } else {
+            println!("Nothing FOund");
+        }
+    }
+}
